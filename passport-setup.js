f65548171868e077725e83e0b429d7fbebd5277b
Mrinal Raj <raj.mrnl@gmail.com>
2018-03-17 03:25:48 +0530

    fixed

--- passport-setup.js
+++ passport-setup.js
@@ -3,15 +3,15 @@ const passport = require('passport'),
     GoogleStrategy = require('passport-google-oauth20'),
     User = require('./models/user-model')
 
-// passport.serializeUser((user, done) => {
-//     done(null,user.id)
-// })
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-// passport.deserializeUser((id,done)=>{
-//     User.findById(id).then((user)=>{
-//         done(err,user)
-//     })
-// })
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -20,10 +20,9 @@ passport.use(
         callbackURL: "/auth/success"
     }, (accessToken, refreshToken, profile, done) => {
 
-        User.findOne({ googleID: profile.id }).then((err,currentUser) => {
-            if(err) return console.log(err)
+        User.findOne({ googleID: profile.id }).then((currentUser) => {
             if (currentUser) {
-                done(null,currentUser)
+                done(null, currentUser)
             }
             else {
                 let photos = profile.photos[0],
